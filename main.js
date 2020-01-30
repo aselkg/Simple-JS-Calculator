6929c55971a14a834eb859adef5ec4ff56b97588
aselkg <aselekkg@gmail.com>
2020-01-30 11:23:16 -0500

    some not working code

---
+++ main.js
@@ -0,0 +1,17 @@
+// document.getElementById("one").addEventListener("click", function () {
+//     document.getElementById("two").innerHTML = "Hello World";
+// });
+
+
+
+let numberOfDrumButtons = document.querySelectorAll(".box").length;
+let = document.getElementById("screen");
+
+let drumButtons = document.querySelectorAll(".box");
+
+function myFunction() {
+    for (var i = 0; i < numberOfDrumButtons; i++) {
+        drumButtons[i].addEventListener("click", function () {
+            drumButtons.innerHTML =
+        })
+    }
