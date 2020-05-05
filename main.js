9e997ad14c0fa144a94b1e38ec3753d52d17dce5
aselkg <aselekkg@gmail.com>
2020-05-04 21:05:10 -0400

    update

--- main.js
+++ main.js
@@ -1,17 +1,69 @@
-// document.getElementById("one").addEventListener("click", function () {
-//     document.getElementById("two").innerHTML = "Hello World";
-// });
 
+var queue = []
+var input = 0
+function calculateQueue (value) {
+  if (input !== 0) {
+    input = parseFloat(input)
 
+    addToQueue(input)
+  }
+  var answer = value[0]
+  var dividedByZero = 0
+  for (var i = 2; i < value.length; i = i + 2) {
+    switch (queue[i - 1]) {
+      case '+':
+        answer += value[i]
+        break
+      case '-':
+        answer -= value[i]
+        break
+      case '/': if (value[i] === 0) dividedByZero = 1
+      else answer = answer / value[i]
 
-let numberOfDrumButtons = document.querySelectorAll(".box").length;
-let = document.getElementById("screen");
-
-let drumButtons = document.querySelectorAll(".box");
-
-function myFunction() {
-    for (var i = 0; i < numberOfDrumButtons; i++) {
-        drumButtons[i].addEventListener("click", function () {
-            drumButtons.innerHTML =
-        })
+        break
+      case '*': answer = answer * value[i]
+        break
     }
+  }
+
+  answer = answer.toFixed(10)
+  answer = parseFloat(answer)
+  if (dividedByZero === 1) {
+    clearAll()
+    document.getElementById('answer').innerHTML = 'ERROR'
+  } else {
+    document.getElementById('answer').innerHTML = answer
+    input = answer
+    queue = []
+  }
+}
+function addToQueue (input) {
+  queue.push(input)
+}
+function clearAll () {
+  queue = []
+  input = 0
+  document.getElementById('answer').innerHTML = '0'
+}
+function number (arg) {
+  if (document.getElementById('answer').innerHTML === 'ERROR' || (document.getElementById('answer').innerHTML == '0' && arg != '.')) { document.getElementById('answer').innerHTML = '' }
+
+  if (!(arg === '.') || !input.match(/[.]/)) {
+    input += arg
+    document.getElementById('answer').innerHTML += arg
+  }
+}
+function operator (arg) {
+  if (input !== 0 && input !== '-') {
+    input = parseFloat(input)
+    addToQueue(input)
+    addToQueue(arg)
+    document.getElementById('answer').innerHTML += arg
+    input = 0
+  }
+  if (arg == '-' && isNaN(queue[0]) && input !== '-') {
+    input = '-'
+
+    document.getElementById('answer').innerHTML = '-'
+  }
+}
